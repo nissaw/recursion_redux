db25f9b514c9eb152b6d25f4bbb186e1ac97d162
nissaw <nissa.wollum@gmail.com>
2015-10-05 15:57:06 -0700

    Stringify cleaned up

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,44 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  if (typeof obj === 'string') {
+  	return '"' + obj + '"'; // add delivery quotes
+  }
+  if (Array.isArray(obj)) {
+  	var result = [];
+  	for (var i = 0; i < obj.length; i++) {
+  		result.push(stringifyJSON(obj[i]));
+  	}
+  	return '[' + result.join(',') + ']';
+  }
+  if (obj && typeof obj === 'object') {
+  	var result = [];
+//alt start  	
+		for (var key in obj) {
+  		if ( stringifyJSON(obj[key]) ) {
+  			result.push( stringifyJSON(key) + ':' + stringifyJSON(obj[key]) );
+  		}		
+		}
+		return '{' + result.join(',') + '}';
+  }
+
+  if (typeof obj === 'function' || typeof obj === 'undefined') {
+  	return;
+  }
+
+ 	return "" + obj;
 };
+// alternate 
+// 	for (var key in obj) {
+//   	if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
+//   		continue;
+//   	} 
+// 		result.push( stringifyJSON(key) + ':' + stringifyJSON(obj[key]) );
+//   		}		
+// 		}
+// 		return '{' + result.join(',') + '}';
+//   }
+
+//  	return "" + obj;
+// };
